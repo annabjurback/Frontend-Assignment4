29176b3cd63f69c528c8bc817e281cd7b6d2a84a
Max Hansen <meepmopo@gmail.com>
2023-03-07 14:37:23 +0100

    Testing  1 2 3

--- Budget/src/main.ts
+++ Budget/src/main.ts
@@ -1,6 +1,13 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+// import App from './App.vue'
 
-import './assets/main.css'
 
-createApp(App).mount('#app')
+// import './assets/main.css'
+
+const app = createApp({
+    data() {
+        return{
+            counter: 0
+        } 
+    }
+}).mount('#app')
