a9cf53953b9dbc80d2e3b9ee3cbeaf14964f6ea7
annabjurback <annabjurback@gmail.com>
2023-03-14 15:47:07 +0100

    four tests working

--- e2e/tests.spec.ts
+++ e2e/tests.spec.ts
@@ -20,8 +20,8 @@ test('ensure expense name input', async ({ page }) => {
   // Fill input fields.
   await inputName.fill('Coffee'); 
   await inputAmount.fill('30');
-  // går det att välja via value på selecten?
   await inputCategory.selectOption('Household'); 
+
   // Commit inputs
   await page.keyboard.press('Enter');
 
@@ -30,7 +30,7 @@ test('ensure expense name input', async ({ page }) => {
   // Get expense name for last item
   let lastExpenseName = await lastItem.locator('.expense-name').textContent();
   
-  // Expect the last expense to contain coffee.
+  // Expect the last expense to equal 'Coffee'.
   await expect(lastExpenseName).toEqual('Coffee');
 });
 
@@ -41,8 +41,45 @@ test ('ensure cost sliders sync', async ({page}) => {
   let minSliderValue = await page.locator('#minimumSlider');
   let maxSliderValue = await page.locator('#maximumSlider');
 
+  // Set maximum slider value, and then minimum slider value
   await maxSliderValue.fill("50");
   await minSliderValue.fill("100");
 
+  // Ensure maximum slider value was set to minimum value (since 100 > 50)
   await expect(await maxSliderValue.inputValue()).toEqual('100');
+});
+
+test ('ensure edit expense', async ({page}) => {
+  await page.goto('http://localhost:5174/');
+
+  // Get input fields.
+  let inputName = await page.locator('#input-name-field');
+  let inputAmount = await page.locator('#input-amount-field');
+  let inputCategory = await page.locator('#input-category-select');
+  
+  // Fill input fields.
+  await inputName.fill('Coffee'); 
+  await inputAmount.fill('30');
+  await inputCategory.selectOption('Household'); 
+
+  // Commit inputs
+  await page.keyboard.press('Enter');
+
+  // Get last list item
+  let lastItem = await page.locator('ul li').last();
+
+  // Click edit button
+  await lastItem.locator('.edit-button').click();
+
+  // Get and fill input field for edit mode
+  await lastItem.locator('.expense-label').fill('edited coffee');
+
+   // Commit edit
+   await page.keyboard.press('Enter');
+
+   // Get expense name for last item
+  let lastExpenseName = await lastItem.locator('.expense-name').textContent();
+    
+  // Expect the last expense to equal 'Edited coffee' (we have a capitalize-function).
+  await expect(lastExpenseName).toEqual('Edited coffee');
 });
