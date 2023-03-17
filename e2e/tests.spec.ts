38d87615cd7545ec200381109c11d4ae76f369da
annabjurback <annabjurback@gmail.com>
2023-03-17 11:52:23 +0100

    Update tests.spec.ts
    Now works even if there are no expenses in the list (by adding an expense in each test that requires an expense present)

--- e2e/tests.spec.ts
+++ e2e/tests.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   // Finns det ett bättre sätt? funkar inte rakt av med liveserver
-  await page.goto('http://localhost:5174/');
+  await page.goto('http://localhost:5173/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Budget/);
@@ -10,17 +10,17 @@ test('has title', async ({ page }) => {
 
 
 test('ensure expense name input', async ({ page }) => {
-  await page.goto('http://localhost:5174/');
+  await page.goto('http://localhost:5173/');
 
   // Get input fields.
   let inputName = await page.locator('#input-name-field');
   let inputAmount = await page.locator('#input-amount-field');
   let inputCategory = await page.locator('#input-category-select');
-  
+
   // Fill input fields.
-  await inputName.fill('Coffee'); 
+  await inputName.fill('Coffee');
   await inputAmount.fill('30');
-  await inputCategory.selectOption('Household'); 
+  await inputCategory.selectOption('Household');
 
   // Commit inputs
   await page.keyboard.press('Enter');
@@ -29,38 +29,48 @@ test('ensure expense name input', async ({ page }) => {
   let lastItem = await page.locator('ul li').last();
   // Get expense name for last item
   let lastExpenseName = await lastItem.locator('.expense-name').textContent();
-  
+
   // Expect the last expense to equal 'Coffee'.
   await expect(lastExpenseName).toEqual('Coffee');
 });
 
-test ('ensure cost sliders sync', async ({page}) => {
-  await page.goto('http://localhost:5174/');
+test('ensure cost sliders sync', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
+
+  // Add one expense to ensure that cost sliders can be manipulated
+  let inputName = await page.locator('#input-name-field');
+  await inputName.fill('Placeholder');
+  let inputAmount = await page.locator('#input-amount-field');
+  await inputAmount.fill('150');
+  let inputCategory = await page.locator('#input-category-select');
+  await inputCategory.selectOption('Household');
+  await page.keyboard.press('Enter');
 
   // Get slider values
   let minSliderValue = await page.locator('#minimumSlider');
   let maxSliderValue = await page.locator('#maximumSlider');
 
-  // Set maximum slider value, and then minimum slider value
+  // Set maximum slider value to 50
   await maxSliderValue.fill("50");
+  // Set minimum slider value to 100
   await minSliderValue.fill("100");
 
   // Ensure maximum slider value was set to minimum value (since 100 > 50)
   await expect(await maxSliderValue.inputValue()).toEqual('100');
 });
 
-test ('ensure edit expense', async ({page}) => {
-  await page.goto('http://localhost:5174/');
+test('ensure edit expense', async ({ page }) => {
+  await page.goto('http://localhost:5173/');
 
   // Get input fields.
   let inputName = await page.locator('#input-name-field');
   let inputAmount = await page.locator('#input-amount-field');
   let inputCategory = await page.locator('#input-category-select');
-  
+
   // Fill input fields.
-  await inputName.fill('Coffee'); 
+  await inputName.fill('Coffee');
   await inputAmount.fill('30');
-  await inputCategory.selectOption('Household'); 
+  await inputCategory.selectOption('Household');
 
   // Commit inputs
   await page.keyboard.press('Enter');
@@ -74,12 +84,12 @@ test ('ensure edit expense', async ({page}) => {
   // Get and fill input field for edit mode
   await lastItem.locator('.expense-label').fill('edited coffee');
 
-   // Commit edit
-   await page.keyboard.press('Enter');
+  // Commit edit
+  await page.keyboard.press('Enter');
 
-   // Get expense name for last item
+  // Get expense name for last item
   let lastExpenseName = await lastItem.locator('.expense-name').textContent();
-    
+
   // Expect the last expense to equal 'Edited coffee' (we have a capitalize-function).
   await expect(lastExpenseName).toEqual('Edited coffee');
 });
